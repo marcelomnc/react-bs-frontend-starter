c45e8d520730387957b42695eb95f41776ea8b1d
marcelomnc <marcelo@pig.gi>
2020-06-17 14:04:55 -0500

    Starter components addition. :bowtie:

---
+++ src/components/About/About.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+const About = (props) => {
+	return (
+		<div className="About">
+			<h2>ABOUT</h2>
+			<p>
+				Sunt proident aute ullamco deserunt nisi tempor pariatur exercitation.
+				Ut quis et culpa enim minim tempor mollit. Nulla minim do minim dolor
+				Lorem id minim culpa consequat tempor. Eiusmod sint exercitation officia
+				magna. Ea anim nulla incididunt incididunt excepteur sit ut nisi
+				consequat nulla sit voluptate commodo. Est aliquip do ipsum
+				reprehenderit irure sint velit elit est nostrud et commodo. Ea amet
+				nostrud sunt aliqua non. Amet amet mollit exercitation sint consequat
+				qui excepteur. Cillum commodo excepteur aliqua est nostrud occaecat
+				nostrud amet ex aliquip. Fugiat in ipsum id minim esse pariatur aute
+				laboris anim ut magna non aliqua. Et sit officia est enim et dolore
+				reprehenderit sint id enim culpa pariatur dolor incididunt. Eiusmod
+				irure elit sunt officia non minim est. Incididunt nostrud nostrud duis
+				ut adipisicing dolor magna incididunt incididunt Lorem. Qui deserunt
+				fugiat anim et et. Laborum ipsum sint nulla cillum velit sint mollit
+				commodo ad pariatur. Cupidatat sunt anim sint qui. Magna deserunt ut
+				aliquip ullamco ullamco excepteur pariatur duis incididunt cillum ea
+				fugiat eiusmod. Deserunt sunt laborum aute nisi. Reprehenderit amet
+				mollit anim adipisicing proident. In dolore voluptate occaecat pariatur
+				laborum ad do reprehenderit. Mollit laborum Lorem consectetur deserunt
+				est labore nisi. Aliqua occaecat culpa id et id nulla do. Aute velit
+				fugiat cillum reprehenderit excepteur. Pariatur voluptate deserunt
+				occaecat deserunt. Anim esse esse et nostrud in consectetur. Duis
+				aliquip ut ipsum esse anim tempor. Labore eiusmod et dolor et id duis
+				reprehenderit elit qui irure. Eu eu in consectetur ad dolor est non et
+				quis irure proident tempor fugiat. Proident laborum dolore tempor
+				aliquip cillum minim sit Lorem culpa ullamco qui. Ullamco ullamco minim
+				eiusmod do deserunt adipisicing. Magna nulla nulla duis cupidatat nulla
+				anim sint nostrud ad velit quis velit. Do do ipsum amet consectetur
+				eiusmod ea nostrud ex ex excepteur non nulla. Velit cupidatat officia
+				irure quis occaecat ipsum ullamco dolore nostrud Lorem aute tempor
+				laboris. Nulla labore ea proident incididunt. Non laborum velit culpa
+				consequat ut labore ex consectetur exercitation excepteur non pariatur
+				nostrud. Quis labore ipsum esse eiusmod consequat enim. Esse ex commodo
+				exercitation elit culpa. Ea id consectetur proident do qui est irure
+				dolor do. Magna culpa ea in laborum aliqua. Aliqua laboris nostrud
+				eiusmod dolore magna nostrud ex incididunt aute deserunt amet. Nisi
+				nulla occaecat veniam reprehenderit dolor id culpa mollit velit tempor
+				ut ullamco proident. Deserunt amet enim labore elit quis. Non
+				exercitation enim voluptate Lorem. Est duis consequat excepteur culpa
+				tempor. Culpa magna ex aute fugiat eu in mollit mollit dolor magna amet.
+				Amet fugiat dolore non veniam. Do consectetur minim magna reprehenderit
+				excepteur incididunt officia.
+			</p>
+		</div>
+	);
+};
+
+export default About;
---
+++ src/components/Dummy/Dummy.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Dummy = (props) => {
+	return (
+		<div className="About">
+			<h2>DUMMY</h2>
+			<p>
+				Mollit est exercitation commodo proident labore ut consectetur magna qui
+				deserunt sunt nostrud voluptate tempor. Ad eu cupidatat occaecat cillum
+				minim laboris veniam voluptate dolore ea. Sint non ex officia ullamco
+				nulla esse amet enim cillum fugiat id exercitation in. Sunt nisi sunt do
+				veniam. Pariatur est mollit reprehenderit aute aliquip ipsum minim
+				minim. Tempor sunt esse Lorem est incididunt Lorem nostrud tempor velit.
+				Reprehenderit enim velit tempor laborum ea. Et non laboris culpa nulla
+				elit pariatur consectetur deserunt adipisicing ea proident consequat
+				labore laboris. Sit sit do magna amet sunt qui fugiat magna anim labore.
+				Proident Lorem cillum consequat magna occaecat cillum adipisicing ea
+				ipsum pariatur esse ex. Quis minim cupidatat Lorem amet velit. Cupidatat
+				exercitation est esse laborum velit eu reprehenderit nostrud elit
+				officia. Excepteur qui irure enim occaecat quis id quis ullamco eiusmod
+				duis ad consectetur occaecat. Eu ullamco enim exercitation labore sit
+				anim ex irure amet sit culpa dolore officia exercitation. Officia
+				reprehenderit elit laborum ullamco nostrud quis commodo duis. Ullamco
+				magna ut enim proident ea cupidatat id nisi nostrud. Fugiat adipisicing
+				cupidatat labore quis veniam sit laborum. Aliquip quis ea officia dolor
+				qui pariatur aliquip ex. In dolor minim quis eu ipsum tempor incididunt
+				aute aliquip anim. In irure esse labore excepteur ipsum proident aliquip
+				proident ut cillum id ex fugiat commodo. Et eiusmod adipisicing
+				excepteur sunt culpa dolor magna nostrud ut ut excepteur magna est
+				laboris. Sunt occaecat reprehenderit eiusmod voluptate cupidatat.
+				Ullamco veniam amet id id aliqua qui officia non anim qui consectetur
+				mollit ipsum fugiat.
+			</p>
+		</div>
+	);
+};
+
+export default Dummy;
